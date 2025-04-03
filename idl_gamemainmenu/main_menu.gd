5517aaed195672fc63f4e84a20c3edcaad11912e
OneHashBrown319 <jesonrivera50@gmail.com>
2025-04-03 12:18:46 -0700

    Title screen
    This title screen

---
+++ idl_gamemainmenu/main_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_play_pressed():
+	pass # replace with get_tree().change_scene_to_file("file name"),
+
+
+func _on_options_pressed():
+	pass 
+
+
+func _on_quit_pressed():
+	get_tree().quit()
